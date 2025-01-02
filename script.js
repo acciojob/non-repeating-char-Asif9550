f8fe64eb1c9f1e08002ddb039ec9826805ee781c
Shaik Asif <bhanurshaikasif@gmail.com>
2025-01-02 19:55:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+  // Create an object to store the count of each character
+  const charCount = {};
+
+  // Loop through the string and count occurrences of each character
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+    charCount[char] = (charCount[char] || 0) + 1;
+  }
+
+  // Loop through the string again to find the first character with a count of 1
+  for (let i = 0; i < str.length; i++) {
+    if (charCount[str[i]] === 1) {
+      return str[i];
+    }
+  }
+
+  // If no non-repeated character is found, return null
+  return null;
 }
+
+// Do not change the code below
 const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+alert(firstNonRepeatedChar(input));
